9fd1afb2fa8f50b932f0a522001d3bd818b8b0cc
Agus Barizi <m3gaplazma@gmail.com>
2022-02-21 13:13:07 +0700

    [ref] implement wire dependency injection

--- main.go
+++ main.go
@@ -1,31 +1,21 @@
 package main
 
 import (
-	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"m3gaplazma/go-restapi/app"
-	"m3gaplazma/go-restapi/controller"
 	"m3gaplazma/go-restapi/helper"
 	"m3gaplazma/go-restapi/middleware"
-	"m3gaplazma/go-restapi/repository"
-	"m3gaplazma/go-restapi/service"
 	"net/http"
 )
 
-func main() {
-	db := app.NewDB()
-	validate := validator.New()
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	categoryController := controller.NewCategoryController(categoryService)
-
-	router := app.NewRouter(categoryController)
-
-	server := http.Server{
+func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "127.0.0.1:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: authMiddleware,
 	}
+}
 
+func main() {
+	server := InitializeServer()
 	err := server.ListenAndServe()
 	helper.PanicIfError(err)
 }
